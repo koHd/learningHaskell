7607a4feb65019715cc5b5851849ff163666dbe8
Shane Farrell <shane.farrell@yahoo.ie>
2016-08-05 17:30:34 +0100

    deleting a dead file

--- interpreter.hs
+++
@@ -1,9 +0,0 @@
-data Tree = Leaf Int | Node Op Tree Tree
-    deriving Show
-
-data Op = Add | Mult
-    deriving Show
-
-reduce (Leaf n) = n
-reduce (Node Add left right) = (reduce left) + (reduce right)
-reduce (Node Mult left right) = (reduce left) * (reduce right)

e1f6f4f15c74e9706157f7bf181a4ac46f1b72ac
Shane Farrell <shane.farrell@yahoo.ie>
2016-08-05 19:27:41 +0100

    Full game working with random secret word.
    The random word comes from enable1.txt

--- hangman/hangman.hs
+++ hangman/hangman.hs
@@ -8,15 +8,26 @@
 	:: else the player loses a life
 	: go to next round -}
 	
+import System.IO
+import System.Random
+	
 -- later on the I/O functionality will go in main to make the game playable from command line
 main = do
+	handle <- openFile "enable1.txt" ReadMode
+	contents <- hGetContents handle
+	gen <- getStdGen
+	let words = lines contents
+	let (n, _) = randomR (0, (length words) - 1) gen :: (Int, StdGen)
+	let secretWord = words !! n
 	putStrLn "Let's Play Hangman!"
-	playHangman 0 7 "secret" []
+	playHangman 0 7 secretWord []
+	hClose handle
+
 
 -- a round of hangman
 playHangman :: (Show a, Ord a, Num a) => a -> a -> [Char] -> [Char] -> IO ()
 playHangman _ _ [] _ = error "no secret word set"
-playHangman _ 0 _ _ = putStrLn "You didn't get the secret this time. Better luck next time!"
+playHangman _ 0 secret _ = putStrLn ("You didn't get the secret this time. It was: " ++ secret)
 playHangman roundNum lives secret guesses 
 	| secret == known = do
 		putStrLn ("You Win! The word is: " ++ known)
@@ -24,6 +35,7 @@ playHangman roundNum lives secret guesses
 		putStrLn "-----"
 		putStrLn ("Round: " ++ (show roundNum))
 		putStrLn ("Known so far: " ++ known)
+		putStrLn ("Guesses so far: " ++ guesses)
 		putStrLn ("Lives: " ++ (show lives))
 		putStrLn "Make a guess: "
 		guess <- getLine
